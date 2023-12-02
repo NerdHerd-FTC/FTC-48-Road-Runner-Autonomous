b4876d720e85a36c8ed266a9e307aa653e687a9c
Cindy Yam <cindyyam8@gmail.com>
2023-12-02 07:46:35 -0800

    Create _2023_12_01_Servo_Test.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/_2023_12_01/_2023_12_01_Servo_Test.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.drive._2023_12_01;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "Claw Test", group = "2023-12-01")
+public class _2023_12_01_Servo_Test extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Servo clawTop = hardwareMap.servo.get("clawTop");
+        clawTop.setDirection(Servo.Direction.FORWARD);
+        clawTop.scaleRange(0, 1);
+
+        waitForStart();
+        clawTop.setPosition(0.3);
+
+        while (opModeIsActive()) {
+            if (gamepad1.a) {
+                clawTop.setPosition(0);
+                wait(500);
+                clawTop.setPosition(0.5);
+                wait(500);
+                clawTop.setPosition(1);
+            }
+
+            telemetry.addData("Claw Top Position: ", clawTop.getPosition());
+            telemetry.update();
+        }
+    }
+}
