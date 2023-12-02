df40c3c6c8b46e5039ad0dd44881a72c614a95e0
Cindy Yam <cindyyam8@gmail.com>
2023-12-02 07:55:52 -0800

    Update _2023_12_01_Drone_Servo_Test.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/_2023_12_01/_2023_12_01_Drone_Servo_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/_2023_12_01/_2023_12_01_Drone_Servo_Test.java
@@ -3,11 +3,11 @@ package org.firstinspires.ftc.teamcode.drive._2023_12_01;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "Servo Test", group = "2023-12-01")
-public class _2023_12_01_Servo_Test extends LinearOpMode {
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "Drone Servo Test", group = "2023-12-01")
+public class _2023_12_01_Drone_Servo_Test extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        Servo clawTop = hardwareMap.servo.get("clawTop");
+        Servo clawTop = hardwareMap.servo.get("Drone_Launcher_Servo");
         clawTop.setDirection(Servo.Direction.FORWARD);
         clawTop.scaleRange(0, 1);
 
