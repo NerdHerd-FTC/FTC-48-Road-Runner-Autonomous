670d8a9692b1feddb8057aa40486bba2db0cfa66
Kavi-Gupta <69233899+Kavi-Gupta@users.noreply.github.com>
2024-01-08 14:13:01 -0800

    fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Cindy/_20231216_RED_DriverOriented_NoToggles.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Cindy/_20231216_RED_DriverOriented_NoToggles.java
@@ -8,12 +8,17 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.drive.MecanumDrivebaseInstance;
+import org.firstinspires.ftc.teamcode.drive.PoseStorage;
+import org.firstinspires.ftc.teamcode.drive.StandardTrackingWheelLocalizer;
 import org.firstinspires.ftc.teamcode.mechanisms.arm.ArmInstance;
 import org.firstinspires.ftc.teamcode.mechanisms.claw.ClawInstance;
 import org.firstinspires.ftc.teamcode.mechanisms.drone_launcher.DroneLauncherInstance;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @TeleOp(name = "RED Driver Oriented - No Toggles")
-@Disabled
 public class _20231216_RED_DriverOriented_NoToggles extends LinearOpMode {
 
     private int Arm_Adjustment_Value = 50;
@@ -22,6 +27,15 @@ public class _20231216_RED_DriverOriented_NoToggles extends LinearOpMode {
     double armSpeed = 0.15;
     @Override
     public void runOpMode() throws InterruptedException {
+        MecanumDrivebaseInstance MecanumDrivebase = new MecanumDrivebaseInstance(hardwareMap);
+
+        List<Integer> lastTrackingEncPositions = new ArrayList<>();
+        List<Integer> lastTrackingEncVels = new ArrayList<>();
+
+        StandardTrackingWheelLocalizer Localizer = new StandardTrackingWheelLocalizer(hardwareMap, lastTrackingEncPositions, lastTrackingEncVels);
+
+        PoseStorage.currentPose = MecanumDrivebase.getPoseEstimate();
+
         ArmInstance Arm = new ArmInstance();
         ClawInstance Claw = new ClawInstance();
         DroneLauncherInstance DroneLauncher = new DroneLauncherInstance();
@@ -40,33 +54,30 @@ public class _20231216_RED_DriverOriented_NoToggles extends LinearOpMode {
         frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
         backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
 
-        IMU imu = hardwareMap.get(IMU.class, "imu");
-
-        // Adjust the orientation parameters to match your robot
-        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
-                RevHubOrientationOnRobot.UsbFacingDirection.UP));
-        // Without this, the REV Hub's orientation is assumed to be logo up / USB backward
-        imu.initialize(parameters);
-
         boolean armDown = true;
 
         waitForStart();
 
         while (opModeIsActive()) {
+            Localizer.update();
+
+            Pose2d RobotPose = Localizer.getPoseEstimate();
+
             double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
             double x = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
 
+            double RobotHeading = RobotPose.getHeading();
+
+
             if (gamepad1.back) {
-                imu.resetYaw();
+                RobotHeading = 0;
             }
 
-            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
 
             // Rotate the movement direction counter to the bot's rotation
-            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
-            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+            double rotX = x * Math.cos(-RobotHeading) - y * Math.sin(-RobotHeading);
+            double rotY = x * Math.sin(-RobotHeading) + y * Math.cos(-RobotHeading);
 
             rotX = rotX * 1.1;  // Counteract imperfect strafing
 
